25bf1b98920f0ed518fe3cd1501961bb7bb93676
CynethiaSm <Cynethiasmith@gmail.com>
2024-02-17 04:01:15 -0800

    Create buttons Add and Restart for progress bar at the top of page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
 /* 
 ------------------------------------------------------------------------------------------------------------------------
-1. Show and Hide Left Side Panel 
-y-axis
+1. Add to and Restart progress bar
+------------------------------------------------------------------------------------------------------------------------
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    const progressBar = document.getElementById('progress-bar');
+    const addMoreButton = document.querySelector('.add-button');
+
+    addMoreButton.addEventListener('click', function () {
+         var currentValue = parseFloat(progressBar.value);
+         var newValue = currentValue + 5;
+
+         newValue = Math.min(newValue, parseFloat(progressBar.max));
+    
+         //Update the value
+        progressBar.value = newValue
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const progressBar = document.getElementById('progress-bar');
+    const restartButton = document.querySelector('.restart-button');
+
+    restartButton.addEventListener('click', function () {
+        
+        //Restart Progress
+        progressBar.value = 0;
+    });
+});
+
+/* 
+------------------------------------------------------------------------------------------------------------------------
+2. Add to and Restart progress bar
+------------------------------------------------------------------------------------------------------------------------
+*/
+
+
+/* 
+------------------------------------------------------------------------------------------------------------------------
+3. Show and Hide Left Side Panel - Main Labels
 ------------------------------------------------------------------------------------------------------------------------
 */
 
@@ -35,16 +72,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /* 
 ------------------------------------------------------------------------------------------------------------------------
-2. VERTICAL SCROLLBAR 
-y-axis
+4. Show and Hide Left Side Panel - Layer 1 Namespaces
 ------------------------------------------------------------------------------------------------------------------------
 */
+document.addEventListener('DOMContentLoaded', function () {
+    const unityEngineNamespaceOne = document.querySelector('.unity-engine-namespace1');
+    const unityEngineSublayerOne = document.querySelector('.unity-engine-sublayer1');
 
+    unityEngineNamespaceOne.addEventListener('click', function () {
+        unityEngineSublayerOne.classList.toggle('hidden');
+    });
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+    const unityEngineNamespaceTwo = document.querySelector('.unity-engine-namespace2');
+    const unityEngineSublayerTwo = document.querySelector('.unity-engine-sublayer2');
+
+    unityEngineNamespaceTwo.addEventListener('click', function () {
+        unityEngineSublayerTwo.classList.toggle('hidden');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const unityEngineNamespaceShareOne = document.querySelectorAll('.unity-engine-nspshare1')
+    const unityEngineNamespaceShareTwo = document.querySelectorAll('.unity-engine-nspshare2')
+
+    unityEngineNamespaceShareOne.forEach(function(element) {
+        element.addEventListener('click' , function () {
+            unityEngineNamespaceShareTwo.forEach(function(element) {
+                element.classList.toggle('hidden');
+            });
+        });
+    });
+});
 
 /* 
 ------------------------------------------------------------------------------------------------------------------------
-3. VERTICAL SCROLLBAR 
-y-axis
+5. Show and Hide Left Side Panel - Layer 2 Class,Enum
 ------------------------------------------------------------------------------------------------------------------------
 */
