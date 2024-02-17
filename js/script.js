1051895e698833d8ea39415f5ebc812bd23d53d1
CynethiaSm <Cynethiasmith@gmail.com>
2024-02-16 23:16:58 -0800

    Include a new folder js for script.js file to make right side toggle list work. Also add to style.css to help hide list.

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/* 
+------------------------------------------------------------------------------------------------------------------------
+1. Show and Hide Left Side Panel 
+y-axis
+------------------------------------------------------------------------------------------------------------------------
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    const unityEngineLabel = document.querySelector('.unity-engine-label1');
+    const unityEngineSubmenu = document.querySelector('.unity-engine-submenu1');
+
+    unityEngineLabel.addEventListener('click', function () {
+        unityEngineSubmenu.classList.toggle('hidden');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const unityEngineLabelTwo = document.querySelector('.unity-engine-label2');
+    const unityEngineSubmenuTwo = document.querySelector('.unity-engine-submenu2');
+
+    unityEngineLabelTwo.addEventListener('click', function () {
+        unityEngineSubmenuTwo.classList.toggle('hidden');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const unityEngineLabelThree = document.querySelector('.unity-engine-label3');
+    const unityEngineSubmenuThree = document.querySelector('.unity-engine-submenu3');
+
+    unityEngineLabelThree.addEventListener('click', function () {
+        unityEngineSubmenuThree.classList.toggle('hidden');
+    });
+});
+
+
+/* 
+------------------------------------------------------------------------------------------------------------------------
+2. VERTICAL SCROLLBAR 
+y-axis
+------------------------------------------------------------------------------------------------------------------------
+*/
+
+
+
+/* 
+------------------------------------------------------------------------------------------------------------------------
+3. VERTICAL SCROLLBAR 
+y-axis
+------------------------------------------------------------------------------------------------------------------------
+*/
